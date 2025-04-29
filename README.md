7fc362b9601dd0370a47d9803e73c836012b47d3
shahabas <mohdshahabasm@gmail.com>
2025-04-29 20:59:44 +0300

    setup and installation completed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Youtube-sentiment-analysis
+
+1. conda create -n youtube python=3.11 -y
+
+2. conda activate youtube
+
+3. pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="YouTube",
+    version="0.0.1",
+    author="shahabas",
+    author_email="mohdshahabasm@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
